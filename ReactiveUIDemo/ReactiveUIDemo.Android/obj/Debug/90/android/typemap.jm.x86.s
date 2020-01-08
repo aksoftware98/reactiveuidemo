72224f3d0bd0dbca3b2d2fbb673582aac0f9cf38
aksoftware98 <aksoftware98@hotmail.com>
2020-01-08 22:41:02 +0200

    Data Binding and StringToColor converter

---
+++ ReactiveUIDemo/ReactiveUIDemo.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 016c4c03d49f4e58d2478adf400d7ed2b40e8551 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1293
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 338767
+	.include	"typemap.jm.inc"
---
+++ ReactiveUIDemo/ReactiveUIDemo/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ReactiveUIDemo
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (value != _username)
+                {
+                    _username = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Username))); 
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
